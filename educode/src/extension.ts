a4e21f3ec8ab347c8cb6ba2614794cd2b388554f
mihirp11 <114366288+mihirp11@users.noreply.github.com>
2025-02-21 09:36:50 -0500

    add code to hit server

--- educode/src/extension.ts
+++ educode/src/extension.ts
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-
+import { suggestSnippet } from './suggest';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -13,15 +13,21 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('educode.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('educode.helloWorld', async () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from EduCode!');
+		const res = await fetch('http://localhost:8000/suggest');
+		const body = await res.json() as {Response: string};
+		console.log(res.status, body["Response"]);
 		const editor = vscode.window.activeTextEditor;
 		if (editor) {
 			const document = editor.document;
 			const text = document.getText();
-			console.log(text);
+			const res = await fetch('http://localhost:8000/logs', {method: 'POST', body: JSON.stringify({code:text, fileName: document.fileName, position: editor.selection.active}), headers: {'Content-Type': 'application/json'}});
+			const body = await res.json() as {response: string}[];
+			console.log(res.status, body.map(a=>a["response"]));
+			suggestSnippet(editor,body[0].response, editor.selection.active);
 			console.log(document.fileName);
 		}else{
 			console.log("No active editor");
--- server/db/router.py
+++ server/db/router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.post('/logs', status_code=201)
+@router.post('/logs', status_code=200)
 def log_data(data: dict):
     return [{"response": "Storing some data", "created": "2025-02-19 10:33:00 AM", "data": data}]
 
