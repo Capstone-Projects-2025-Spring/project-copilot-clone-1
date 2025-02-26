31c35887061714e927fb79cd6791b7f934b0ad19
mihirp11 <114366288+mihirp11@users.noreply.github.com>
2025-02-25 22:50:06 -0500

    create InlineSuggestionsProvider and tests

--- educode/src/extension.ts
+++ educode/src/extension.ts
@@ -8,30 +8,45 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "educode" is now active!');
-
+	const suggestor:vscode.InlineCompletionItemProvider = {
+		provideInlineCompletionItems: async(document, position, context, token) => {
+			const res = await fetch('http://localhost:8000/suggest');
+			const json = await res.json() as {Response: string};
+			const suggestion = json["Response"];
+			 const range = new vscode.Range(position.translate(0, -2), position);
+			// const text = document.getText(range);
+			console.log("provideInlineCompletionItems Called");
+			return {
+				items: [{
+					insertText: suggestion,
+				},{insertText: "print('Hello')"}]
+			};
+		}
+	};
+	vscode.languages.registerInlineCompletionItemProvider({pattern: "**"}, suggestor);
 	// The command "Hello World" has been defined in the package.json file
 	// Implementation of the command is providedwith registerCommand
 	// The commandId parameter must match the command field in package.json
 	const disposable = vscode.commands.registerCommand('educode.helloWorld', async () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
+		
 		vscode.window.showInformationMessage('Hello World from EduCode!');
-		//fetch suggestion from the fastAPI server
-		const res = await fetch('http://localhost:8000/suggest');
-		const body = await res.json() as {Response: string};
-
-		console.log(res.status, body["Response"]);
+		
+		console.log("suggestor registered");
+	
 		const editor = vscode.window.activeTextEditor;
 		if (editor) {
 			const document = editor.document;
 			const text = document.getText();
 			//Post log of file content, file name and cursor position to the fastAPI server
-			const res = await fetch('http://localhost:8000/logs', {method: 'POST', body: JSON.stringify({code:text, fileName: document.fileName, position: editor.selection.active}), });
-			const body = await res.json() as {response: string}[];
+			const postRes = await fetch('http://localhost:8000/logs', {method: 'POST', body: JSON.stringify({code:text, fileName: document.fileName, position: editor.selection.active}) });
+			const postBody = await postRes.json() as {detail:{response: string}[]};
+			console.log(postBody);
 			//log response from the fastAPI server
-			console.log(res.status, body.map(a=>a["response"]));
-			suggestSnippet(editor,body[0].response || "no suggestion from server", editor.selection.active);
-			//log to test if the editor is active
+			console.log(postRes.status);
+			// suggestSnippet(editor,suggestion, editor.selection.active);
+			// log to test if the editor is active
 			console.log(document.fileName);
 		}else{
 			console.log("No active editor");
