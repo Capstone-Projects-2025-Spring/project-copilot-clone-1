5125ab41b74b7e6f21f862de33248f16769c7243
larryb02 <77469185+larryb02@users.noreply.github.com>
2025-02-27 01:51:36 +0000

    removed response_model param from decorator for now

--- server/ai/router.py
+++ server/ai/router.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
-from openai.types import Completion
+# from openai.types import Completion
 
 router = APIRouter()
 
-@router.get('/suggest', status_code=200, response_model=Completion)
+@router.get('/suggest', status_code=200)
 async def get_suggestion() -> dict:
     return {"Response": "Here's some code..."}
