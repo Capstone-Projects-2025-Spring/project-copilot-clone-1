ae257f7df51b67460f1a1f7b1f240d29bd3994ef
larryb02 <77469185+larryb02@users.noreply.github.com>
2025-02-27 01:08:30 +0000

    created tests

---
+++ server/tests/db_routes.py
@@ -0,0 +1,21 @@
+from db import router
+from db.models import CodeSnippet
+from fastapi.testclient import TestClient
+from datetime import datetime
+from main import app
+
+client = TestClient(app)
+
+
+def test_read_logs():
+    response = client.get("/logs")
+    assert response.status_code == 200
+
+
+def test_write_logs():
+    body = CodeSnippet(
+        _id=None, userId="", language="", code="", createdAt=datetime.today()
+    ).model_dump_json()
+    print(body)
+    response = client.post("/logs", json=body)
+    assert response.status_code == 200
