c07ccb1ddf2b1b60235090e139097261e703fa27
rten19 <tup71747@temple.edu>
2025-03-02 14:45:16 -0500

    Updated the testing for getting suggestions from the LLM from using .get() to .post()

--- server/tests/ai_routes.py
+++ server/tests/ai_routes.py
@@ -1,9 +1,15 @@
 from ai import router
 from fastapi.testclient import TestClient
 from main import app
+from ai.models import CodeRequest, SuggestionResponse
 
 client = TestClient(app)
 
 def test_get_suggestions():
-    response = client.get("/suggest")
+    ##response = client.get("/suggest")
+    body = CodeRequest(
+        code="#This function computes Matrix multiplication\ndef matrix_mul(a,b)",
+        instructions="Complete the code. Do not add any comments and keep function as concise as possible.",
+    ).model_dump()
+    response = client.post('/suggest', json=body)
     assert response.status_code == 200
