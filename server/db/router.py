eddb54d5f1c0c2e1ee3c670b2c9e2c98c78ef7de
larryb02 <77469185+larryb02@users.noreply.github.com>
2025-03-18 19:57:02 +0000

    added general descriptions of routes

--- server/db/router.py
+++ server/db/router.py
@@ -13,12 +13,16 @@ COLLECTION_NAME = os.environ.get("MONGO_COLLECTION_NAME")
 
 router = APIRouter()
 database = Database(uri=URI, db_name=DB_NAME, collection_name=COLLECTION_NAME)
-
+"""
+Write some data to a collection in database
+"""
 @router.post('/logs', status_code=200)
 def log_data(data: CodeSnippet):
     database.send_code_snippet(data.userId, data.language, data.code)
     return [{"response": "Storing some data", "data": data, "created": data.createdAt}]
-
+"""
+Read from a collection in database
+"""
 @router.get('/logs', status_code=200)
 def read_logs():
     return [{"_id": "complexObject_FF294F", "timestamp": "2025-02-19 10:32:00 AM", "accepted": "false"}]
