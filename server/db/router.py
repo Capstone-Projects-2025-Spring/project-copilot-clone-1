54ee1c22a633c2065c26fbdc91765d20bea4e443
larryb02 <77469185+larryb02@users.noreply.github.com>
2025-03-19 19:55:04 +0000

    added status codes for log data route

--- server/db/router.py
+++ server/db/router.py
@@ -1,10 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from db.models import CodeSnippet
 from db.db import Database
 from dotenv import load_dotenv
 import os
 
-env_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '.env')
+env_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".env")
 load_dotenv(dotenv_path=env_path)
 
 URI = os.environ.get("MONGO_URI")
@@ -13,16 +13,30 @@ COLLECTION_NAME = os.environ.get("MONGO_COLLECTION_NAME")
 
 router = APIRouter()
 database = Database(uri=URI, db_name=DB_NAME, collection_name=COLLECTION_NAME)
-"""
-Write some data to a collection in database
-"""
-@router.post('/logs', status_code=200)
+
+
+@router.post("/logs", status_code=200)
 def log_data(data: CodeSnippet):
-    database.send_code_snippet(data.userId, data.language, data.code)
-    return [{"response": "Storing some data", "data": data, "created": data.createdAt}]
-"""
-Read from a collection in database
-"""
-@router.get('/logs', status_code=200)
+    """
+    Write data to a collection in database
+    """
+    try:
+        # add input verification here
+        database.send_code_snippet(data.userId, data.language, data.code)
+        return [{"response": "Storing some data", "data": data, "created": data.createdAt}]
+    except Exception as e:
+        raise HTTPException(status_code=500)
+
+
+@router.get("/logs", status_code=200)
 def read_logs():
-    return [{"_id": "complexObject_FF294F", "timestamp": "2025-02-19 10:32:00 AM", "accepted": "false"}]
+    """
+    Read from a collection in database
+    """
+    return [
+        {
+            "_id": "complexObject_FF294F",
+            "timestamp": "2025-02-19 10:32:00 AM",
+            "accepted": "false",
+        }
+    ]
